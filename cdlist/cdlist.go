948363794f6329b4de51663f6b1ae6feb5df2852
Ron Corral <rcorral@corral.com>
2018-04-18 22:44:47 +0000

    initial commit

---
+++ cdlist/cdlist.go
@@ -0,0 +1,101 @@
+package main
+
+// Creates the files that contain the html that displays 3 CD covers based on the type of list
+// It's either 3 random covers or the 3 CDs that have the latest date_added
+
+// standard packages
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"os"
+)
+
+// packages written by me
+import (
+	"common"
+)
+
+func main() {
+
+	var (
+		artistid     int
+		cdartist     string
+		namemodifier int
+		coverfile    string
+		cdtitle      string
+	)
+	var htmlCode bytes.Buffer
+	var orderBy string
+	var listFile string
+
+	domainPtr := flag.String("domain", "", "domain CD list is for")
+	listTypePtr := flag.String("type", "", "type of CD list (random or latest)")
+	flag.Parse()
+
+	domain := *domainPtr
+	listType := *listTypePtr
+
+	if len(domain) == 0 {
+		flag.Usage()
+		log.Fatal("ERROR: A domain name is required")
+	}
+
+	// Just sanitizing the data since it is user input
+	if common.IsAlphaNumeric(domain) == false {
+		flag.Usage()
+		log.Fatal("ERROR: I only buy domains with numbers and letters")
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("/var/www/cdfiles/%s", domain)); os.IsNotExist(err) {
+		flag.Usage()
+		log.Fatal("ERROR: I don't use that domain:", domain)
+	}
+
+	switch listType {
+	case "random":
+		orderBy = "rand()"
+		listFile = "randcd.txt"
+	case "latest":
+		orderBy = "date_added desc"
+		listFile = "whatsnewcd.txt"
+	default:
+		flag.Usage()
+		log.Fatal("ERROR: type either missing or not valid type: ", listType)
+
+	}
+
+	sql := fmt.Sprintf("SELECT distinct cdtitle.artist_id as artistid, cdartist.artist as cdartist, cdartist.name_action as namemodifier, cdtitle.cover_file as coverfile, cdtitle.title as cdtitle FROM cdtitle right join cdartist on cdartist.artist_id = cdtitle.artist_id where cdartist.artist != 'Compilations' and cdartist.artist != 'Soundtracks' order by %s limit 3", orderBy)
+
+	db := common.OpenDB()
+	defer db.Close()
+
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&artistid, &cdartist, &namemodifier, &coverfile, &cdtitle)
+		if err != nil {
+			log.Fatal(err)
+		}
+		print_this_name := html.EscapeString(common.TruncateString(common.DisplayableName(cdartist, namemodifier), 11))
+		print_this_title := html.EscapeString(cdtitle)
+		path_to_cover := html.EscapeString(fmt.Sprintf("/images/covers/%s/%s", coverfile[0:1], coverfile))
+		switch listType {
+		case "random":
+			htmlCode.WriteString(fmt.Sprintf("<li><a href=\"/cdcoll.php?artist_id=%d\"><img class=\"roundcorner\" src=\"%s\" height=\"64\" width=\"64\" alt=\"%s\" title=\"%s\"></a><p>%s</p></li>\n", artistid, path_to_cover, print_this_title, print_this_title, print_this_name))
+		case "latest":
+			htmlCode.WriteString(fmt.Sprintf("<li><img class=\"roundcorner imagepointer\" src=\"%s\" height=\"64\" width=\"64\" alt=\"%s\" title=\"%s\" onclick=\"getArtistData(%d,'%s')\"><p>\"%s\"</p></li>\n", path_to_cover, print_this_title, print_this_title, artistid, domain, print_this_name))
+		}
+	}
+	// This section needs to be updated to include error checking and first writing to temp file so I don't blow
+	// away current good file with possible currupted data.
+	f, err := os.Create(fmt.Sprintf("/var/www/cdfiles/%s/%s", domain, listFile))
+	_, err = f.WriteString(htmlCode.String())
+	f.Sync()
+}
---
+++ common/common.go
@@ -0,0 +1,101 @@
+package common
+
+// Contains functions that are used in multiple programs or thought I was going to use in multiple programs
+
+// Standard packages
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// Downloaded packages
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DisplayableName(name string, modifier int) string {
+	// DisplayableName - Handles the hacky way many years go I dealt with how artist name is stored in the DB vs
+	// how it is displayed and haven't come up with something better so I just rewrite this in
+	// every language I use.
+	//
+	// 0 - leave alone unless there is a comma then swap first 2 words around comma, leave rest alone
+	//        "Cale, John / Lou Reed / Nico" -> "John Cale / Lou Reed / Nico"
+	//        "Ridgway, Stan" -> "Stan Ridgway"
+	//        "Wire" -> "Wire"
+	// 1 - leave alone even though there is a comma in the name
+	//        "Not Drowning, Waving" -> "Not Drowning, Waving"
+	// 2 - Deals with someone having a middle initial
+	//        "Bach, Johann Sebastian" -> "Johann Sebastian Bach"
+	// 4 - add "The " at beginning
+	//        "Velvet Underground" -> "The Velvet Underground"
+	if modifier == 4 {
+		return fmt.Sprintf("The %s", name)
+	} else if modifier == 1 {
+		return name
+	} else if strings.Contains(name, ",") && (strings.Count(name, " ") == 1 || modifier == 2) {
+		n := strings.Split(name, ", ")
+		return fmt.Sprintf("%s %s", n[1], n[0])
+	} else if strings.Contains(name, ",") && strings.Count(name, " ") > 1 {
+		n := strings.Split(name, ", ")
+		r := strings.SplitN(n[1], " ", 2)
+		return fmt.Sprintf("%s %s %s", r[0], n[0], r[1])
+	}
+	return name
+}
+
+func IsAlphaNumeric(s string) bool {
+	// IsAlphaNumeric - Just sanitizing user entered string
+
+	for _, r := range s {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func TruncateString(str string, maxLen int) string {
+	// TruncateString - truncates artist name so hopefully it's not wider than the cover image
+	// maxLen is usually 11
+	shortString := str
+	if len(str) > maxLen {
+		shortString = str[0:maxLen-3] + "..."
+	}
+	return shortString
+}
+
+func DBCredentials() (string, string, string) {
+	// DBCredentials - returns db credentials from environment
+
+	dbname := os.Getenv("DBNAME")
+	if len(dbname) == 0 {
+		log.Fatal("DBNAME not defined")
+	}
+	dbpass := os.Getenv("DBPASS")
+	if len(dbpass) == 0 {
+		log.Fatal("DBPASS not defined")
+	}
+	dbuser := os.Getenv("DBUSER")
+	if len(dbuser) == 0 {
+		log.Fatal("DBUSER not defined")
+	}
+	return dbname, dbpass, dbuser
+}
+
+func OpenDB() *sql.DB {
+	// OpenDB - Opens a connection to the DB and returns pointer to it
+
+	dbname, dbpass, dbuser := DBCredentials()
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		dbuser, dbpass, "127.0.0.1", "3306", dbname))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
